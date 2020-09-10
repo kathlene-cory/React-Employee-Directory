f14f21072a890c96ca268515f017007393638219
Kathlene Cory <Kathlene.cory@gmail.com>
2020-09-10 13:26:08 -0600

    Updates

---
+++ precache-manifest.183726550df81ce57464bd04380a89f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "437a95048f7fe5aa244615c30e6086d3",
+    "url": "/React-Employee-Directory/index.html"
+  },
+  {
+    "revision": "74d4dadf15e8c912b80e",
+    "url": "/React-Employee-Directory/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "737d3862f0de0e2d12b4",
+    "url": "/React-Employee-Directory/static/js/2.a780f940.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Employee-Directory/static/js/2.a780f940.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74d4dadf15e8c912b80e",
+    "url": "/React-Employee-Directory/static/js/main.56c0ed77.chunk.js"
+  },
+  {
+    "revision": "d5a550fc769b1b6308d1",
+    "url": "/React-Employee-Directory/static/js/runtime-main.f187368d.js"
+  }
+]);
